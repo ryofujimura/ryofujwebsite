aec9857e5384736f69376e9fc573c6d9d7266c8c
Mitchell Christ <xzmaru@gmail.com>
2022-01-24 23:22:26 -0800

    add basic layout

---
+++ src/lib/Toolbar.svelte
@@ -0,0 +1,9 @@
+<nav>
+	toolbar
+</nav>
+
+<style>
+	nav {
+		@apply border-gray-300 md:border-r <md:border-b;
+	}
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,33 @@
-<slot></slot>
+<div class="root">
+	<Toolbar/>
+	<Explorer/>
+
+	<main>
+		<slot></slot>
+	</main>
+</div>
+
+<style>
+	.root {
+		display: grid;
+	}
+
+	@screen md {
+		.root {
+			grid-template-columns: 4rem 16rem 1fr;
+			height: 100vh;
+		}
+	}
+
+	@screen <md {
+		.root {
+			grid-template-rows: repeat(3, auto);
+		}
+	}
+</style>
 
 <script>
+	import Toolbar from '$lib/Toolbar.svelte'
+	import Explorer from '$lib/Explorer.svelte'
 	import '../app.css'
 </script>
