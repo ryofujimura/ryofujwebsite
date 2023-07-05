48d850d6dab319d81df5970b0101da47878b3d2b
Ryo Fujimura <ryo176999@gmail.com>
2023-07-04 23:02:35 -0700

    reset

--- src/lib/work/Work.svelte
+++
@@ -1,9 +0,0 @@
-<article>
-	<a class="link" href="/works/{work.id}">
-		{work.title}
-	</a>
-</article>
-
-<script lang="ts">
-	export let work: Work
-</script>
--- src/routes/__error.svelte
+++
@@ -1,9 +0,0 @@
-<SEO {title} />
-
-<h1>404 — {title}</h1>
-
-<script lang="ts">
-	import SEO from '$lib/SEO.svelte'
-
-	let title = 'Page not found'
-</script>
