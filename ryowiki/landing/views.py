9bab731d24852bf2c12730c62a68da33c1c013cb
Ryo Fujimura <ryo176999@gmail.com>
2023-09-11 23:14:55 -1000

    0.1.3
    Tabs introduced

--- ryowiki/landing/views.py
+++ ryowiki/landing/views.py
@@ -4,6 +4,9 @@ from datetime import datetime
 def index(request):
     return render(request, "landing/index.html")
 
+def tab1(request):
+    return render(request, 'landing/tab1.html')
+
 def calculate_age(birthdate):
     today = datetime.today()
     age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
