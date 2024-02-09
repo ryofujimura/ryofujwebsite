a66bd5ed9fe9bab7397901f49ec46ad1016e952d
Ryo Fujimura <ryo176999@gmail.com>
2024-02-08 21:19:08 -0800

    deleted

--- cusousashopify/script.js
+++
@@ -1,6 +0,0 @@
-
-    // Add any JavaScript functionality here, e.g., handling quantity changes, adding to cart, etc.
-    document.querySelector('button').addEventListener('click', function() {
-        // Replace this with your actual logic for adding to cart
-        alert('Product added to cart!');
-    });
