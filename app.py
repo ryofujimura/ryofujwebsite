0f88a6f8fb012c6ee61182d1d1835176f74a998b
Ryo Fujimura <ryo176999@gmail.com>
2024-05-01 00:45:00 -0700

    python experience list

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    projects = [
+        {'title': 'Honda', 'image': 'honda.svg', 'description': 'Data management and analysis'},
+        {'title': 'Project 2', 'image': 'image2.png', 'description': 'Description of Project 2'},
+        # Add as many projects as you like
+    ]
+    return render_template('index.html', projects=projects)
 
 if __name__ == '__main__':
     app.run(debug=True)
