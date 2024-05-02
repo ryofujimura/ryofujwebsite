0b664ecb568d6619003f54d1f2e2d8783b9f8f9d
Ryo Fujimura <ryo176999@gmail.com>
2024-05-01 21:18:15 -0700

    description changed to role. description added as description.
    image not showing bug fized

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, abort
+import os
+from flask import Flask, render_template, abort, url_for
 
 app = Flask(__name__)
 
@@ -12,22 +13,20 @@ hyperlink = [
     {'title': 'Resume', 'url': 'https://drive.google.com/file/d/1TdtNaYYYiZngj8SL2CL7R8NwLqFlHUCD/view?usp=sharing', 'icon': 'M22 0h-20v24h14l6-6v-18zm-7 23h-12v-22h18v16h-6v6zm1-5h4.586l-4.586 4.586v-4.586zm-3 1h-8v1h8v-1zm0-3h-8v1h8v-1zm6-2v-1h-14v1h14zm0-4h-4v1h4v-1zm-6.006 1h-7.991l-.003-.789c-.003-.72-.006-1.615 1.314-1.92 1.483-.341 1.236-.418 1.158-.563-1.078-1.988-.71-3.173-.395-3.703.388-.651 1.089-1.025 1.923-1.025.827 0 1.523.368 1.91 1.011.545.904.409 2.222-.379 3.713-.105.196-.195.255 1.119.559 1.355.312 1.352 1.212 1.35 1.936l-.006.781zm-6.994-1h6c-.007-.547-.07-.626-.54-.734-.855-.198-1.629-.376-1.901-.972-.142-.311-.113-.66.087-1.039.61-1.151.758-2.146.407-2.729-.276-.458-.778-.526-1.053-.526-.48 0-.857.19-1.063.537-.352.59-.201 1.58.414 2.714.204.377.236.727.095 1.039-.269.598-1.036.774-1.847.962-.525.121-.593.202-.599.748zm13-2v-1h-4v1h4zm0-4h-4v1h4v-1z'},
 ]
 projects = [
-    {'id': 'honda', 'title': 'Work', 'image': 'honda.svg', 'description': 'Data Management<br>and Analysis'},
-    {'id': 'cusco', 'title': 'Work', 'image': 'cusco.svg', 'description': 'Data Engineer'},
-    {'id': 'matchatime', 'title': 'Project', 'image': 'matchatime.svg', 'description': 'macOS app<br>Swift, SwfitUI'},
-    
+    {'id': 'honda', 'company': 'American Honda Motor Company, Inc.', 'title': 'Work', 'image': 'honda.svg', 'role': 'Data Management<br>and Analysis', 'description': 'I worked on a project to improve the data management and analysis process for a large automotive company.'},
+    {'id': 'cusco', 'company': 'CUSCO USA Inc.', 'title': 'Work', 'image': 'cusco.svg', 'role': 'Data Engineer', 'description': 'I worked on a project to improve the data management and analysis process for a large automotive company.'},
+    {'id': 'matchatime', 'company': 'Moyai LLC', 'title': 'Project', 'image': 'matchatime.svg', 'role': 'Developer<br>Project Management', 'description': 'I worked on a project to improve the data management and analysis process for a large automotive company.'}, 
 ]
 
 @app.route('/')
 def home():
-    return render_template('index.html', name = name, title = title, description = description, hyperlink = hyperlink, projects = projects)
-
+    return render_template('index.html', name = name, title = title , hyperlink = hyperlink, projects = projects)
 
 @app.route('/projects/<project_id>')
 def project_page(project_id):
     project = next((p for p in projects if p['id'] == project_id), None)
-    if project is not None:
-        # Assuming the images are stored in the 'static/img-asset' directory
+    if project:
+        # Generate image paths
         image_paths = [f"img-asset/{project['id']}_{i}.jpg" for i in range(1, 4)]
         return render_template('project_detail.html', project=project, images=image_paths)
     else:
