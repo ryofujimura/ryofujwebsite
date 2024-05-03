a420d8d73b7dc6cbc9fc80593f5f79b617efcf10
Ryo Fujimura <ryo176999@gmail.com>
2024-05-03 00:23:55 -0700

    homepage-profile, icon showing only if exists

--- app.py
+++ app.py
@@ -72,8 +72,9 @@ featured_skills = ["swift", "database", "python"]
 
 # Get available images with their extensions
 img_asset_path = os.path.join(app.static_folder, 'img-asset')
-available_images = [filename.split('.')[1] for filename in os.listdir(img_asset_path)]
-
+# for each file in the img_asset_path, add "img-asset/" in front 
+available_images = [f'img-asset/{f}' for f in os.listdir(img_asset_path) if os.path.isfile(os.path.join(img_asset_path, f))]
+print(available_images)
 # Define routes
 @app.route('/')
 def index():
